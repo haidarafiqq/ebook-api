75fd241564b3c789090e13d5efe495cf47704aa8
haidarafiqq <3103120102@student.smktelkom-pwt.sch.id>
2022-10-24 08:53:13 +0700

    migration

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Siswa;
+
+class SiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $siswa = Siswa::all();
+        return $siswa;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $store = Siswa::create($request->all());
+        return $store;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $update = Siswa::where("id", $id)->update($request->all());
+        return $update;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $destroy = Siswa::destroy($id);
+        return $destroy;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\heloController;
+use App\Http\Controllers\SiswaController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+/*
+* Task
+* URL : http://localhost/apiq/halo
+* METHOD: GET
+* Exec: function
+* Return: JSON => {"me": "Ganteng"}
+*/
+// Route::get('halo', function(){
+//     return ["me" => "ganteng"];
+
+//     //return $data;
+// });
+
+// Route::resource('helo', [heloController::clas, 'index']);
+
+Route::resource('helocontroller', heloController::class);
+Route::resource('siswa', SiswaController::class);
+Route::resource('book', BookController::class); 
